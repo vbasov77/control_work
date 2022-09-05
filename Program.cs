d499fafc64c609ec5cd63a5776eda9d42f2e9741
Vbasov <0120912@mail.ru>
2022-09-05 13:44:27 +0300

    add program

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+string[] firstArray = new string[4] {"hello", "2", "world", ":-)"};
+string[] secondArray = new string[firstArray.Length];
+void SecondArray(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+    if(firstArray[i].Length <= 3)
+        {
+        secondArray[count] = firstArray[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(firstArray, secondArray);
+PrintArray(secondArray);
